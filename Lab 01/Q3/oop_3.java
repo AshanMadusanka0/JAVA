2a6497b130bada44900708313a81f4d8a9f3307c
AshanMadusanka0 <136890819+AshanMadusanka0@users.noreply.github.com>
2023-06-18 22:54:09 +0530

    Add files via upload

---
+++ Lab 01/Q3/oop_3.java
@@ -0,0 +1,11 @@
+
+
+package com.mycompany.oop_3;
+
+public class oop_3 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Executing Loop " + i);
+        }
+    }
+}
